64aeec725088ef1ad045f7ffe29f05febaa1b1b0
se0lhee <gs05074@naver.com>
2019-03-22 13:42:07 +0900

    add src

--- src/main/java/org/seolhee/jdbc/template/ArticleDaoImplUsingTemplate.java
+++ src/main/java/org/seolhee/jdbc/template/ArticleDaoImplUsingTemplate.java
@@ -4,7 +4,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
-import javax.sql.DataSource;
+import javax.sql.DataSource; 
 
 import org.mariadb.jdbc.MariaDbDataSource;
 import org.seolhee.jdbc.raw.Article;
